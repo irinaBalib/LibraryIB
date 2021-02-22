3ae0542bc3222cc6127d860cafe3c6d30d95ac4a
irinaBalib <irinaBalib@users.noreply.github.com>
2021-02-22 21:58:52 +0200

    library

---
+++ Library/Library.Web/Controllers/LibraryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Library.Logic;
+using Library.Data;
+
+
+namespace Library.Web.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private static BookManager manager = new BookManager();
+        public IActionResult Index()
+        {
+            var books = manager.GetAvailableBooks();
+            return View(books);
+        }
+
+        //for viewing user's list 
+        public IActionResult UserBooks()
+        {
+            var books = manager.GetUserBooks();
+            return View(books);
+        }
+
+        public IActionResult Take(string title)
+        {
+            manager.TakeBook(title);
+           return RedirectToAction(nameof(UserBooks));
+        }
+
+    }
+}
---
+++ Library/Library/Logic/BookManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Library.Data;
+
+namespace Library.Logic
+{
+    public class BookManager
+    {
+        /*  
+         ------ GetAvailableBooks() - returns list of all available books
+        ------ GetUserBooks() - returns list of user's books
+        ------ TakeBook(string title) - gives the book to the user
+        ------ ReturnBook(string title) - returns the book back to the library
+        */
+        private UserBooks User { get; set; }
+        private LibraryBooks Library { get; set; }
+
+        public BookManager()
+        {
+            User = new UserBooks();
+            Library = new LibraryBooks();
+        }
+
+        public List<Book> GetAvailableBooks()
+        {
+            return Library.Books.Where(b => b.Copies > 0).OrderBy(b => b.Title).ToList();
+        }
+        public List<Book> GetUserBooks()
+        {
+            return User.Books.ToList();
+        }
+        public Book TakeBook(string title)
+        {
+            // Take - user takes a book providing its title. Validation if book exists. Validation if book is
+            // still available(check number of copies). Book is added to the user's list and available
+            // book count is decreased.
+
+            var book = Library.Books.Find(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (book != null && book.Copies > 0)
+            {
+                book.Copies--;
+                User.Books.Add(book);
+
+                return book;
+            }
+
+            return null;
+        }
+        public void ReturnBook(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+    }
+
+}
